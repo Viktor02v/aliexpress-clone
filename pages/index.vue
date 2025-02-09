eb3cf0bc7de0b2de0ac32ae9ff00dbc00e42ea15
Viktor Petruk <damnnn261@gmail.com>
2025-02-09 09:27:41 +0200

    create (ProductComponent.vue),([id].vue, start work on it)

--- pages/index.vue
+++ pages/index.vue
@@ -87,11 +87,10 @@ const products = [
 
 <template>
 	<MainLayout>
-		<div d="IndexPage" class="mt-5 max-w-[1200px] mx-auto px-2">
+		<div d="IndexPage" class="mt-5 max-w-[1200px] h-full mx-auto px-2">
 
 			<div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
-				<div v-if="products" v-for="product in products" :key="product">
-					<!-- {{ product }} -->
+				<div v-for="product in products" :key="product.id">
 					<ProductComponent :product="product" />
 				</div>
 			</div>
