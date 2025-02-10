a24de9030983c03179c542ef9aa2a31dd003ce73
Viktor Petruk <damnnn261@gmail.com>
2025-02-10 21:07:59 +0200

    make the functionality for (CartItem.vue)

---
+++ components/CartItem.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+import { useUserStore } from "@/stores/user.store";
+
+const route = useRoute();
+
+const userStore = useUserStore();
+
+const props = defineProps(['product', 'selectedArray'])
+
+const { product, selectedArray } = toRefs(props)
+
+const emit = defineEmits(['selectedRadio'])
+
+let isHover = ref(false)
+let isSelected = ref(false)
+
+const removeFormCart = () => {
+	userStore.cart.forEach((prod, index) => {
+		if (prod.id === product?.value.id) {
+			userStore.cart.splice(index, 1);
+		}
+	})
+}
+
+watch(() => isSelected.value, (val) => {
+	emit('selectedRadio', { id: product?.value.id, val: val })
+})
+</script>
+
+<template>
+	<div class="flex justify-start my-2">
+	</div>
+</template>
+
+<style scoped></style>
