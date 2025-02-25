c778a8b112c04a57edc238ce15ab65f7098f9197
Viktor Petruk <damnnn261@gmail.com>
2025-02-25 10:00:51 +0200

    make functionality in (app.vue)

--- app.vue
+++ app.vue
@@ -1,5 +1,55 @@
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user.store'
+const userStore = useUserStore()
+
+const route = useRoute()
+
+const windowWidth = ref<number>(0)
+
+const resizeHandler = () => {
+  windowWidth.value = window.innerWidth
+}
+
+onMounted(() => {
+  userStore.isLoading = true
+  window.addEventListener('resize', resizeHandler)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', resizeHandler)
+})
+
+watch(
+  () => windowWidth.value,
+  () => {
+    if (windowWidth.value >= 768) {
+      userStore.isMenuOverlay = false
+    }
+  }
+)
+
+watch(
+  () => route.fullPath,
+  () => {
+    userStore.isLoading = true
+  }
+)
+</script>
 <template>
   <div class="bg-[#F2F2F2] w-full h-full">
     <NuxtPage />
+
+    <MenuOverlay
+      :class="[
+        {
+          'max-h-[100vh] transition-all duration-200 ease-in visible':
+            userStore.isMenuOverlay,
+        },
+        {
+          'max-h-0 transition-all duration-200 ease-in invisible':
+            !userStore.isMenuOverlay,
+        },
+      ]"
+    />
   </div>
 </template>
